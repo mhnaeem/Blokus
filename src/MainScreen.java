6caef2d644d9f3300141a4199d419b69c07d8b75
Sakif-Max-Flex <55748153+Sakif-Max-Flex@users.noreply.github.com>
2019-10-24 14:23:38 -0230

    Updated Version
    Took consideration for making code look clean and added functions to the buttons.

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -3,71 +3,97 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 /**
- * Main Screen for Blokus Game that includes the following:
+ * Main Screen for the Blokus Game that includes the following:
  * Window title;
  * Blokus title on window; and
  * 3 Buttons.
- *
+ * 
  *
  * @author (Zachary Cheema)
- * @version (Version 1.1)
+ * @version (Version 1.3)
  */
 public class MainScreen extends JFrame{
-
-    private JPanel mainPanel;
-    private Container contentPane = getContentPane();
-    private JLabel mainLbl;
-    private JButton b1, b2, b3;
-
-    public MainScreen(){
-        setFrame();
-        createDetails();
-        addDetails();
-        buttonAction();
-        setVisible(true);
-    }
-
-    public void setFrame(){
-        setSize(500,300);
-        setPreferredSize(new Dimension(500,300));
-        setTitle("Welcome to BLOCKUS!");
-        mainPanel = new JPanel();
-        contentPane.add(mainPanel);
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        setResizable(false);
-    }
-
-    public void createDetails(){
+    
+   private JPanel mainPanel;
+   private Container contentPane = getContentPane();
+   private JLabel mainLbl;
+   private JButton startButton, loadButton, exitButton;
+   
+   public MainScreen(){
+       setFrame();
+       createLabel();
+       createButtons();
+       addDetails();
+       buttonAction();
+       setVisible(true);
+   }
+   
+   public void setFrame(){
+       setSize(500,300);
+       setPreferredSize(new Dimension(500,300));
+       setTitle("Welcome to BLOCKUS!");
+       mainPanel = new JPanel();
+       contentPane.add(mainPanel);
+       mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+       setResizable(false);
+   }
+   
+   public void createLabel(){
         mainLbl = new JLabel("BLOKUS");
-        b1 = new JButton("Start");
-        b2 = new JButton("Load");
-        b3 = new JButton("Exit");
-    }
+   }
+   
+   public void createButtons(){
+       startButton = new JButton("Start");
+       loadButton = new JButton("Load");
+       exitButton = new JButton("Exit");
+   }
+   
+   public void addDetails(){
+       Dimension labelPanelSize = new Dimension(500, 150);
+       Dimension BtnPanelsize = new Dimension(200, 50);
+       Dimension btnSize = new Dimension(100, 50);
+       
+       
+       JPanel mainLblPan = new JPanel();
+       mainLblPan.setPreferredSize(labelPanelSize); 
+       mainLblPan.setLayout(new FlowLayout());
 
-    public void addDetails(){
-        Dimension size = new Dimension(500, 150);
-
-        JPanel mainLblPan = new JPanel();
-        mainLblPan.setPreferredSize(size);
-        mainLblPan.setLayout(new FlowLayout());
-
-        JPanel buttonPan = new JPanel();
-        buttonPan.setPreferredSize(size);
-        buttonPan.setLayout(new FlowLayout());
-
-        buttonPan.add(b1);
-        buttonPan.add(b2);
-        buttonPan.add(b3);
+       JPanel buttonPan = new JPanel();
+       buttonPan.setPreferredSize(BtnPanelsize);
+       buttonPan.setLayout(new FlowLayout());
+      
+       
+       startButton.setPreferredSize(btnSize);
+       loadButton.setPreferredSize(btnSize); 
+       exitButton.setPreferredSize(btnSize); 
+       
+        buttonPan.add(startButton);
+        buttonPan.add(loadButton);
+        buttonPan.add(exitButton);
         mainPanel.add(buttonPan);
-
-        mainPanel.add(mainLblPan);
-        mainPanel.add(buttonPan);
-    }
-
-    private void buttonAction(){
-        b3.addActionListener((ActionEvent ev) -> {
+        mainLblPan.add(mainLbl);
+       mainPanel.add(mainLblPan);
+       mainPanel.add(buttonPan);
+   }
+   
+   private void buttonAction(){
+        exitButton.addActionListener((ActionEvent ev) -> {
             System.exit(0);
         });
+        loadButton.addActionListener((ActionEvent ev) -> {
+            dispose();
+            new loadSavedScreen();// This button will load saved files 
+        });
+        startButton.addActionListener((ActionEvent ev) -> {
+            dispose();
+            new CreateGame(); // This Button will bring you to the game setup
+            //screen
+        });
     }
+    
+   public static void main(String[] args) {
+        new MainScreen();
+   }
 }
+
 
