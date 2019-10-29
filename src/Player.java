94e026e23b1c5c83aa85225f464b68ded92b18f0
Muhammad Hammad <mhnaeem@mun.ca>
2019-10-28 22:31:11 -0230

    Static list for all players added

--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,20 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Player {
 
     private String playerName;
     private Color playerColour;
     private ArrayList<Piece> availablePieces;
+    private static ArrayList<Player> players = new ArrayList<>(Arrays.asList());
 
     public Player(String player_name, Color player_color, ArrayList<Piece> player_pieces){
         this.playerName = player_name;
         this.playerColour = player_color;
         this.availablePieces = player_pieces;
+
+        players.add(this);
     }
 
     public String getPlayerName(){
@@ -23,5 +27,9 @@ public class Player {
 
     public ArrayList<Piece> getAvailablePieces(){
         return this.availablePieces;
+    }
+
+    public static ArrayList<Player> getPlayers(){
+        return players;
     }
 }
