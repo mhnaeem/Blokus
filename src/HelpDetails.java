38cf195afe6e73327b7c8773316a2176b05e1f66
zoe c <zscollins@mun.ca>
2019-10-31 21:36:54 -0230

    created help menu

--- src/HelpDetails.java
+++ src/HelpDetails.java
@@ -6,23 +6,68 @@ import javax.swing.*;
 /**
  * Help details displays the information to assist users in playing the Blokus game
  */
-public class HelpDetails extends JFrame 
+public class HelpDetails extends JFrame
 {
-    public HelpDetails()
+    public HelpDetails(String type)
     {
-        // creates how to function for the help menu 
         super("Instructions");
-        setLayout(new FlowLayout());
-        setSize(new Dimension(200,200));
+        setLayout(new BorderLayout());
+        setSize(new Dimension(600,600));
 
-        JTextArea howToText = new JTextArea("To start a game, fill all drop-down boxes with game specifics, then press start!");
+        JLabel titleText = new JLabel("Instructions");
+
+        JLabel contentText = new JLabel("");
+        String message = "";
+
+
+        if (type.equals("create"))
+        {
+            contentText.setText("Starting a Game: ");
+            message += "To start a game, fill all drop-down boxes with game specifics, then press start!\n";
+            setTitle("Setup Instructions");
+
+        }
+
+        if (type.equals("game"))
+        {
+            contentText.setText("Playing a Piece: ");
+            message += "To play a piece, first select it from your tray. \n";
+            message += "You can choose to rotate or flip the piece. \n";
+            message += "You can also pass your turn.\n";
+            message += "Once the piece is oriented correctly, select a location on the game board. \n";
+            message += "This will play your piece. \n";
+            setTitle("Game Instructions");
+        }
+
+        if (type.equals("blokus"))
+        {
+            contentText.setText("Description of the Game: ");
+            message += "In Blokus, players place pieces onto a board, with the goal of having no (or the least number of) pieces at the end.\n";
+            message += "Each player's first move must have a piece placed in a corner of the board.\n";
+            message += "Pieces must be placed next to the corner of another piece of that colour, but with no two sides adjacent.\n";
+            setTitle("Blokus info");
+        }
+
+        JTextArea howToText = new JTextArea(message);
         JScrollPane scrollPane = new JScrollPane(howToText);
         howToText.setEditable(false);
         howToText.setLineWrap(true);
         howToText.setWrapStyleWord(true);
-        getContentPane().add(howToText);
+        add(titleText, BorderLayout.NORTH);
+        add(howToText, BorderLayout.CENTER);
+
+
+        // makes back button
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(ev -> HelpDetails.this.dispose());
+        add(backButton, BorderLayout.SOUTH);
+
         setVisible(true);
     }
+
+//    public static void main(String[] args){
+//        new HelpDetails("blokus");
+//    }
 
 }
 
