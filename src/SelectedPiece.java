fbf13b269fa6f888204c6d87f086bc863f3fa2bc
Muhammad Hammad <mhnaeem@mun.ca>
2019-11-07 05:19:19 -0330

    Refactored SelectedPiece class, now includes different functions for buttons.

--- src/SelectedPiece.java
+++ src/SelectedPiece.java
@@ -1,6 +1,14 @@
-import javax.swing.*;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 
 /**
@@ -13,8 +21,7 @@ import java.util.ArrayList;
 
 public class SelectedPiece{
 
-    private JButton pass, rotate, flipUp, flipRight, back;
-    private JPanel piece, buttons, main;
+    private JPanel piece, main;
     private static JFrame frm;
     private String selectedButtonName;
     private int playerIndex;
@@ -33,55 +40,14 @@ public class SelectedPiece{
 
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
 
-        ImageIcon flipUpIcon = new ImageIcon("./Assets/Icons/iconfinder_multimedia-26_2849810.png");
-        ImageIcon flipRightIcon = new ImageIcon("./Assets/Icons/iconfinder_DoubleChevronLeftRight_1031523.png");
-        ImageIcon closeIcon = new ImageIcon("./Assets/Icons/close.png");
-        ImageIcon rotateIcon = new ImageIcon("./Assets/Icons/rotate.png");
-        ImageIcon passIcon = new ImageIcon("./Assets/Icons/pass.png");
         this.piece = new JPanel();
         this.piece.setBorder(new EmptyBorder(5,5,5,5));
         this.piece.add(createGrid(5,5,50,50));
 
         main.add(this.piece);
 
-        this.rotate = new JButton(rotateIcon);
-        this.rotate.addActionListener(ev -> {
-            Piece.setActionList(rotateCounterClock(Piece.getActionsList(GameEngine.getSelectedPiece())));
-            displayPiece();
-        });
-        this.pass = new JButton(passIcon);
-        this.pass.addActionListener(ev -> {
-            GameEngine.updateCurrentTurn();
-            frm.dispose();
-        });
-        this.flipUp = new JButton(flipUpIcon);
-        this.flipUp.addActionListener(ev -> {
-            Piece.setActionList(flipUp(Piece.getActionsList(GameEngine.getSelectedPiece())));
-            displayPiece();
-        });
-        //this.flipUp.setDisabledIcon(icon);
-        //this.flipUp.setIcon(icon);
-        this.flipRight = new JButton(flipRightIcon);
-        this.flipRight.addActionListener(ev -> {
-            Piece.setActionList(flipRight(Piece.getActionsList(GameEngine.getSelectedPiece())));
-            displayPiece();
-        });
+        createButtons();
 
-        this.back = new JButton(closeIcon);
-        this.back.addActionListener(ev -> {
-            GameEngine.setSelectedPiece(null);
-            frm.dispose();
-        });
-
-        buttons = new JPanel();
-
-        buttons.add(this.pass);
-        buttons.add(this.rotate);
-        buttons.add(this.flipUp);
-        buttons.add(this.flipRight);
-        buttons.add(this.back);
-
-        main.add(buttons);
         frm.add(main);
 
         displayPiece();
@@ -94,9 +60,59 @@ public class SelectedPiece{
         frm.setVisible(true);
     }
 
+    private void createButtons(){
+        JButton pass, rotate, flipUp, flipRight, back;
 
+        //Set the icons for each button
+        ImageIcon flipUpIcon = new ImageIcon("./Assets/Icons/iconfinder_multimedia-26_2849810.png");
+        ImageIcon flipRightIcon = new ImageIcon("./Assets/Icons/iconfinder_DoubleChevronLeftRight_1031523.png");
+        ImageIcon closeIcon = new ImageIcon("./Assets/Icons/close.png");
+        ImageIcon rotateIcon = new ImageIcon("./Assets/Icons/rotate.png");
+        ImageIcon passIcon = new ImageIcon("./Assets/Icons/pass.png");
+
+        //Set the actionListeners for each button
+        rotate = new JButton(rotateIcon);
+        rotate.addActionListener(ev -> {
+            Piece.setActionList(rotateCounterClock(Piece.getActionsList(GameEngine.getSelectedPiece())));
+            displayPiece();
+        });
+
+        pass = new JButton(passIcon);
+        pass.addActionListener(ev -> {
+            GameEngine.updateCurrentTurn();
+            frm.dispose();
+        });
+
+        flipUp = new JButton(flipUpIcon);
+        flipUp.addActionListener(ev -> {
+            Piece.setActionList(flipUp(Piece.getActionsList(GameEngine.getSelectedPiece())));
+            displayPiece();
+        });
+
+        flipRight = new JButton(flipRightIcon);
+        flipRight.addActionListener(ev -> {
+            Piece.setActionList(flipRight(Piece.getActionsList(GameEngine.getSelectedPiece())));
+            displayPiece();
+        });
+
+        back = new JButton(closeIcon);
+        back.addActionListener(ev -> {
+            GameEngine.setSelectedPiece(null);
+            frm.dispose();
+        });
+
+        JPanel buttons = new JPanel();
+        buttons.add(pass);
+        buttons.add(rotate);
+        buttons.add(flipUp);
+        buttons.add(flipRight);
+        buttons.add(back);
+
+        main.add(buttons);
+    }
 
     private JPanel createGrid(int gridRows, int gridColumns, int buttonWidth, int buttonHeight) {
+
         JPanel tempPanel = new JPanel();
         tempPanel.setLayout(new GridBagLayout());
         JButton[][] buttons = createButtonsList(gridRows, gridColumns);
@@ -119,6 +135,7 @@ public class SelectedPiece{
     }
 
     private JButton[][] createButtonsList(int gridRows, int gridColumns) {
+
         JButton[][] buttons = new JButton[gridRows][gridColumns];
         for (int r = 0; r < gridRows; r++) {
             for (int c = 0; c < gridColumns; c++) {
@@ -134,15 +151,17 @@ public class SelectedPiece{
 
 
     private void displayPiece(){
+
         if(selectedButtonGrid != null){
-            for (int i = 0; i < selectedButtonGrid.length; i++) {
-                for (int j = 0; j < selectedButtonGrid[i].length; j++) {
-                    selectedButtonGrid[i][j].setBackground(Color.white);
-                    selectedButtonGrid[i][j].setIcon(null);
-                    selectedButtonGrid[i][j].setDisabledIcon(null);
+            for (JButton[] jButtons : selectedButtonGrid) {
+                for (JButton jButton : jButtons) {
+                    jButton.setBackground(Color.white);
+                    jButton.setIcon(null);
+                    jButton.setDisabledIcon(null);
                 }
             }
         }
+
         //These x and y represent the zero coordinates, all actions will be taken from these points
         int x = 2;
         int y = 2;
@@ -194,6 +213,7 @@ public class SelectedPiece{
         this.piece.updateUI();
         this.main.updateUI();
     }
+
     public static void pieceHasBeenPlacedEvent(){
         frm.dispose();
     }
@@ -207,11 +227,7 @@ public class SelectedPiece{
     private static ArrayList<int[]> flipUp(ArrayList<int[]> actionsList){
         ArrayList<int[]> toReturn = new ArrayList<>();
         actionsList.forEach(action -> {
-            int y = action[1];
-            if (y != 0) {
-                y = -y;
-            }
-            toReturn.add(new int[]{action[0], y});
+            toReturn.add(new int[]{action[0], action[1] * -1});
         });
         return toReturn;
     }
@@ -219,12 +235,7 @@ public class SelectedPiece{
     private static ArrayList<int[]> flipRight(ArrayList<int[]> actionList){
         ArrayList<int[]> toReturn = new ArrayList<>();
         actionList.forEach(action -> {
-            int x = action[0];
-            int y = action[1];
-            if (x != 0){
-                x = -x;
-            }
-            toReturn.add(new int[]{x, y});
+            toReturn.add(new int[]{action[0] * -1, action[1]});
         });
         return toReturn;
     }
