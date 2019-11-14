444e57308f79520bfa112c0d1621762ad423d42e
Muhammad Hammad <mhnaeem@mun.ca>
2019-11-14 16:24:54 -0330

    SaveGame class extended, can now save the main grid.

--- src/SaveGame.java
+++ src/SaveGame.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -8,14 +10,47 @@ import java.util.List;
 
 public class SaveGame {
 
-    private static void createSaveFile(String filename){
-        List<String> lines = Arrays.asList("The first line", "The second line");
+    public static void createSaveFile(String filename){
+        List<String> lines = Arrays.asList("The first line", saveMainGrid());
         Path file = Paths.get("./SavedGames/"+filename);
         try {
             Files.write(file, lines, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String saveMainGrid(){
+        JButton[][] buttonsToSave = MainGrid.getMainGridButtons();
+
+        String toReturn = "main_grid:[";
+
+        for (int r = 0; r < buttonsToSave.length; r++) {
+            for (int c = 0; c < buttonsToSave[r].length; c++) {
+                JButton currButton = buttonsToSave[r][c];
+                toReturn += "[" + "(" + currButton.getName() + ")" + ";" + currButton.isEnabled() + ";" + colourToString(currButton.getBackground()) + "]";
+            }
+        }
+
+        toReturn += "]";
+
+        return toReturn;
+    }
+
+    private static String colourToString(Color colour){
+        if(colour == Color.BLUE){
+            return "blue";
+        }
+        if(colour == Color.red){
+            return "red";
+        }
+        if(colour == Color.green){
+            return "green";
+        }
+        if(colour == Color.yellow){
+            return "yellow";
+        }
+        return "white";
     }
 
     public static void main(String[] args) {
